6084ceba6c3d86c3d214fdcd2710751f5c0a267e
Pintor Correas external <alejandro.pintor-correas.external@capgemini.com>
2023-02-02 15:34:09 +0100

    ejercicio2.5:correccion filtro fecha, y dos primeras validaciones2

--- src/main/java/com/ccsw/tutorial/prestamo/PrestamoRepository.java
+++ src/main/java/com/ccsw/tutorial/prestamo/PrestamoRepository.java
@@ -19,7 +19,7 @@ public interface PrestamoRepository extends CrudRepository<Prestamo, Long> {
     * @return
     */
     Page<Prestamo> findAll(Pageable pageable);
-    //@Query("SELECT p FROM Prestamo p WHERE (:gameId IS NULL OR p.game.id = :gameId) AND (:clientId IS NULL OR p.client.id = :clientId) AND (:fechaPrestamo IS NULL OR (p.fecha_prestamo <= :fechaPrestamo AND p.fecha_devolucion > :fechaPrestamo))")
+
     @Query("select p from Prestamo p where (:gameId is null or p.game.id = :gameId) and (:clientId is null or p.client.id = :clientId) and (:fecha is null  or ( :fecha BETWEEN p.fechaInicio AND p.fechaFin))")
     
     Page<Prestamo> find(Pageable pageable, @Param("gameId") Long gameId, @Param("clientId") Long clientId, @Param("fecha") Date fecha);
